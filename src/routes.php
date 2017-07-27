b6f4589e48584bc67053f2861854abbb788074c1
Matthew Goslett <matthew@goslett.co.za>
2017-07-27 14:16:42 +0200

    Apply fixes from StyleCI (#1)

--- src/routes.php
+++ src/routes.php
@@ -4,7 +4,6 @@ $route = Route::get(
     config('prometheus.metrics_route_path'),
     \Superbalist\LaravelPrometheusExporter\MetricsController::class . '@getMetrics'
 )->name('metrics'); /** @var \Illuminate\Routing\Route $route */
-
 $middleware = config('prometheus.metrics_route_middleware');
 
 if ($middleware) {
